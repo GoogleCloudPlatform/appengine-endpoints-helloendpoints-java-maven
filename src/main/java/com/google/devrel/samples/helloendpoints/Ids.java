77d0257d06f3d5ef12bf67dcd85b1e171d79ab79
Dan Holevoet <danielholevoet@google.com>
2013-05-28 17:15:49 -0700

    Initial source commit
    Reviewed in https://codereview.appspot.com/9702044/

---
+++ src/main/java/com/google/devrel/samples/helloendpoints/Ids.java
@@ -0,0 +1,11 @@
+package com.google.devrel.samples.helloendpoints;
+
+/**
+ * Contains the client IDs for allowed clients consuming the helloworld API.
+ */
+public class Ids {
+  public static final String WEB_CLIENT_ID = "replace this with your web client application ID";
+  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+}
