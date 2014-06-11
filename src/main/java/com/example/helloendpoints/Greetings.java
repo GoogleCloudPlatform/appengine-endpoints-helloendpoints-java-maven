210d93c0f1e4ae32908ff325de025dd9b53d9e9e
Doug Anderson <douga@google.com>
2014-06-11 15:29:54 -0700

    New version based on App Engine Hello Endpoints Maven artifact.

---
+++ src/main/java/com/example/helloendpoints/Greetings.java
@@ -0,0 +1,59 @@
+package com.example.helloendpoints;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.response.NotFoundException;
+import com.google.appengine.api.users.User;
+
+import java.util.ArrayList;
+
+import javax.inject.Named;
+
+/**
+ * Defines v1 of a helloworld API, which provides simple "greeting" methods.
+ */
+@Api(
+    name = "helloworld",
+    version = "v1",
+    scopes = {Constants.EMAIL_SCOPE},
+    clientIds = {Constants.WEB_CLIENT_ID, Constants.ANDROID_CLIENT_ID, Constants.IOS_CLIENT_ID},
+    audiences = {Constants.ANDROID_AUDIENCE}
+)
+public class Greetings {
+
+  public static ArrayList<HelloGreeting> greetings = new ArrayList<HelloGreeting>();
+
+  static {
+    greetings.add(new HelloGreeting("hello world!"));
+    greetings.add(new HelloGreeting("goodbye world!"));
+  }
+
+  public HelloGreeting getGreeting(@Named("id") Integer id) throws NotFoundException {
+    try {
+      return greetings.get(id);
+    } catch (IndexOutOfBoundsException e) {
+      throw new NotFoundException("Greeting not found with an index: " + id);
+    }
+  }
+
+  public ArrayList<HelloGreeting> listGreeting() {
+    return greetings;
+  }
+
+  @ApiMethod(name = "greetings.multiply", httpMethod = "post")
+  public HelloGreeting insertGreeting(@Named("times") Integer times, HelloGreeting greeting) {
+    HelloGreeting response = new HelloGreeting();
+    StringBuilder responseBuilder = new StringBuilder();
+    for (int i = 0; i < times; i++) {
+      responseBuilder.append(greeting.getMessage());
+    }
+    response.setMessage(responseBuilder.toString());
+    return response;
+  }
+
+  @ApiMethod(name = "greetings.authed", path = "hellogreeting/authed")
+  public HelloGreeting authedGreeting(User user) {
+    HelloGreeting response = new HelloGreeting("hello " + user.getEmail());
+    return response;
+  }
+}
---
+++ src/main/java/com/example/helloendpoints/HelloGreeting.java
@@ -0,0 +1,20 @@
+package com.example.helloendpoints;
+
+public class HelloGreeting {
+
+  public String message;
+
+  public HelloGreeting() {};
+
+  public HelloGreeting(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
